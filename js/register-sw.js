c4e9b6c58fce26c2987029cd2a3e94f4eaaa860d
Shikzz88 <santiagosoulieragrometal@gmail.com>
2026-05-18 13:35:39 -0300

    Intento 1
    Intento 1

---
+++ js/register-sw.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log('Service Worker registrado con éxito', reg))
+      .catch(err => console.warn('Error al registrar el Service Worker', err));
+  });
+}
